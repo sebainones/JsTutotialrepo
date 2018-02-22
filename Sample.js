2b69f8c33923ca03be8a9d70fcf32e0beb90ffa1
Sebastián  Inones <sebastian.inones@bcndata.es>
2018-02-22 10:44:17 +0100

    Progreso sobre uso de variables con la palabra let

--- Sample.js
+++ Sample.js
@@ -2,6 +2,17 @@ console.log("Hola");
 
 console.log("Chau");
 
+const a = 5;
+
+
+let x= 4;
+var y = 3;  //Let is similar to var but let is recommended unless you rellay needo to use var
+
 (function SampleFuntion(){
     console.log("Some log");
 })
+
+console.log( 'suma incorrecta: ' + x + y);
+console.log( 'suma correcta :' +(x+y));
+let z =  x +y;
+console.log( 'suma correcta :' +z);
