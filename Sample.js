77016e4b426dde68f9c8730aa2b31ff38100dc9f
Sebastián  Inones <sebastian.inones@bcndata.es>
2018-02-27 14:52:38 +0100

    Un pco más de tipos de datos ya que el tipo de datos está almacenado en la varibale misma

--- Sample.js
+++ Sample.js
@@ -15,4 +15,13 @@ var y = 3;  //Let is similar to var but let is recommended unless you rellay nee
 console.log( 'suma incorrecta: ' + x + y);
 console.log( 'suma correcta :' +(x+y));
 let z =  x +y;
-console.log( 'suma correcta :' +z);
+
+console.log( 'suma correcta :' + z);
+console.log ("tipo de datos de x es : "+ typeof x);
+
+let boolValue = true;
+
+console.log ("tipo de datos de boolValue es : "+ typeof boolValue);
+let variableIndefinida;
+
+console.log ("tipo de datos de variableIndefinida es : "+ typeof variableIndefinida);
